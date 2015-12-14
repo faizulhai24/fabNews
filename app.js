fbf24945a30d31ae2054e931b637cb42cf059627
Faiz <faizulhai24@gmail.com>
2015-12-14 21:43:39 +0530

    Mongo bug fixed

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(passport.initialize());
 app.use('/', routes);
 app.use('/users', users);
 
-mongoose.connect('mongodb://faizulhai24:q1w2e3r4@ds029595.mongolab.com:29595/fapnews');
+mongoose.connect('mongodb://faiz:q1w2e3r4@ds029595.mongolab.com:29595/fapnews');
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
